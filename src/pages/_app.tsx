ba4ac0a7df35c22bc51f80b7514da48d88f5972c
Javier Rodriguez <javicerodriguez@gmail.com>
2023-08-13 18:57:02 -0300

    add: Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <main className="container mx-auto min-h-screen">
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </NextUIProvider>
   );
