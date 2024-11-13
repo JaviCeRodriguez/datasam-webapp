b278a1277ab915670852cfdd473f736c6c8044e5
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-11-13 20:14:15 -0300

    add: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
