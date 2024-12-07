727c55b48f8776fcace7223a209f59cadfd5680f
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-12-07 17:07:30 -0300

    fix: import speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
