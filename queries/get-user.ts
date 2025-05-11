528431f80cce461e6b039938e043fc72b487fe86
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-05-10 22:59:08 -0300

    fix: remove commented-out code in getUser function for clarity

--- queries/get-user.ts
+++ queries/get-user.ts
@@ -2,27 +2,4 @@ import { TypedSupabaseClient } from "@/lib/supabase/utils";
 
 export async function getUser(client: TypedSupabaseClient) {
   return client.auth.getUser();
-  // const {
-  //   data: { user },
-  //   error,
-  // } = await client.auth.getUser();
-
-  // if (error || !user) {
-  //   throw new Error("No authenticated user");
-  // }
-
-  // return client
-  //   .from("user_roles")
-  //   .select(
-  //     `
-  //         user_id,
-  //         role_id,
-  //         roles:role_id (
-  //           id,
-  //           role_name
-  //         )
-  //       `
-  //   )
-  //   .eq("user_id", user.id)
-  //   .single();
 }
