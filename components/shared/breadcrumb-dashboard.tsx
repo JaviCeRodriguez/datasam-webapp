ab525d3207d36e4d7ae1842930a0d1271dd83918
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-05-12 00:00:10 -0300

    feat: implement dynamic breadcrumb navigation in dashboard using pathname

--- components/shared/breadcrumb-dashboard.tsx
+++ components/shared/breadcrumb-dashboard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Fragment } from "react";
 import {
   Breadcrumb,
   BreadcrumbSeparator,
@@ -6,18 +9,43 @@ import {
   BreadcrumbPage,
   BreadcrumbLink,
 } from "../ui/breadcrumb";
+import { usePathname } from "next/navigation";
 
 export function BreadcrumbDashboard() {
+  const pathname = usePathname();
+
+  const formatPath = (path: string) => {
+    return path
+      .split("/")
+      .filter(Boolean)
+      .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1))
+      .slice(1);
+  };
+
+  const getPartialUrl = (segment: string) => {
+    const segments = pathname.split("/");
+    const index = segments.indexOf(segment);
+    return segments.slice(0, index + 1).join("/");
+  };
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem className="hidden md:block">
-          <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+          <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
         </BreadcrumbItem>
-        <BreadcrumbSeparator className="hidden md:block" />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-        </BreadcrumbItem>
+        {formatPath(pathname).map((segment, index) => (
+          <Fragment key={index}>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>
+                <BreadcrumbLink href={getPartialUrl(segment)}>
+                  {segment}
+                </BreadcrumbLink>
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </Fragment>
+        ))}
       </BreadcrumbList>
     </Breadcrumb>
   );
